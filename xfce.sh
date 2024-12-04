b2f09f1fe1b5353665eb693ba39662f84ff912e9
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2024-12-04 09:40:37 +0800

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -2,20 +2,18 @@
 
 username="$1"
 
-pkgs=('git' 'neofetch' 'virglrenderer-android' 'papirus-icon-theme' 'xfce4' 'xfce4-goodies' 'eza' 'pavucontrol-qt' 'bat' 'jq' 'nala' 'wmctrl' 'firefox' 'netcat-openbsd' 'termux-x11-nightly' 'eza')
+pkgs=('git' 'neofetch' 'virglrenderer-android' 'papirus-icon-theme' 'eza' 'pavucontrol-qt' 'bat' 'jq' 'nala' 'wmctrl' 'netcat-openbsd' 'termux-x11-nightly' 'eza')
 
 #Install xfce4 desktop and additional packages
 pkg install "${pkgs[@]}" -y -o Dpkg::Options::="--force-confold"
 
 #Put Firefox icon on Desktop
-cp $PREFIX/share/applications/firefox.desktop $HOME/Desktop 
-chmod +x $HOME/Desktop/firefox.desktop
+#cp $PREFIX/share/applications/firefox.desktop $HOME/Desktop 
+#chmod +x $HOME/Desktop/firefox.desktop
 
 #Set aliases
 echo "
 alias debian='proot-distro login debian --user $username --shared-tmp'
-#alias zrun='proot-distro login debian --user $username --shared-tmp -- env DISPLAY=:1.0 MESA_LOADER_DRIVER_OVERRIDE=zink TU_DEBUG=noconform '
-#alias zrunhud='proot-distro login debian --user $username --shared-tmp -- env DISPLAY=:1.0 MESA_LOADER_DRIVER_OVERRIDE=zink TU_DEBUG=noconform GALLIUM_HUD=fps '
 alias hud='GALLIUM_HUD=fps '
 alias ls='eza -lF --icons'
 alias cat='bat '
@@ -27,19 +25,12 @@ alias list='nala list --upgradeable'
 alias show='nala show'
 " >> $PREFIX/etc/bash.bashrc
 
-#Download Wallpaper
-wget https://raw.githubusercontent.com/wahyu22010/Debian/main/peakpx.jpg
-wget https://raw.githubusercontent.com/wahyu22010/Debian/main/dark_waves.png
-mv peakpx.jpg $PREFIX/share/backgrounds/xfce/
-mv dark_waves.png $PREFIX/share/backgrounds/xfce/
-
 #Install WhiteSur-Dark Theme
-wget https://github.com/vinceliuice/WhiteSur-gtk-theme/archive/refs/tags/2023-04-26.zip
-unzip 2023-04-26.zip
-tar -xf WhiteSur-gtk-theme-2023-04-26/release/WhiteSur-Dark-44-0.tar.xz
-mv WhiteSur-Dark/ $PREFIX/share/themes/
-rm -rf WhiteSur*
-rm 2023-04-26.zip
+wget https://github.com/gorontaloku/Debi4n/raw/refs/heads/main/Qogir.tar.x
+tar -xf Qogir.tar.xz
+mv Qogir/ $PREFIX/share/themes/
+rm Qogir-Dark
+rm Qogir-Light
 
 #Install Fluent Cursor Icon Theme
 wget https://github.com/vinceliuice/Fluent-icon-theme/archive/refs/tags/2023-02-01.zip
@@ -49,33 +40,12 @@ mv Fluent-icon-theme-2023-02-01/cursors/dist-dark $PREFIX/share/icons/
 rm -rf $HOME//Fluent*
 rm 2023-02-01.zip
 
-#Setup Fonts
-wget https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip
-mkdir .fonts 
-unzip CascadiaCode-2111.01.zip
-mv otf/static/* .fonts/ && rm -rf otf
-mv ttf/* .fonts/ && rm -rf ttf/
-rm -rf woff2/ && rm -rf CascadiaCode-2111.01.zip
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Meslo.zip
-unzip Meslo.zip
-mv *.ttf .fonts/
-rm Meslo.zip
-rm LICENSE.txt
-rm readme.md
-
-wget https://github.com/wahyu22010/Debian/raw/main/NotoColorEmoji-Regular.ttf
-mv NotoColorEmoji-Regular.ttf .fonts
-
-wget https://github.com/wahyu22010/Debian/raw/main/font.ttf
-mv font.ttf .termux/font.ttf
-
 #Setup Fancybash Termux
-wget https://raw.githubusercontent.com/wahyu22010/Debian/main/fancybash.sh
-mv fancybash.sh .fancybash.sh
-echo "source $HOME/.fancybash.sh" >> $PREFIX/etc/bash.bashrc
-sed -i "326s/\\\u/$username/" $HOME/.fancybash.sh
-sed -i "327s/\\\h/termux/" $HOME/.fancybash.sh
+#wget https://raw.githubusercontent.com/wahyu22010/Debian/main/fancybash.sh
+#mv fancybash.sh .fancybash.sh
+#echo "source $HOME/.fancybash.sh" >> $PREFIX/etc/bash.bashrc
+#sed -i "326s/\\\u/$username/" $HOME/.fancybash.sh
+#sed -i "327s/\\\h/termux/" $HOME/.fancybash.sh
 
 #Autostart Conky
 wget https://github.com/wahyu22010/Debian/raw/main/config.tar.gz
