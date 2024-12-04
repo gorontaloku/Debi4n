a9795570f3610eb59c578874eecd02b5a32ed604
gorontaloku <106912043+gorontaloku@users.noreply.github.com>
2024-12-04 10:05:15 +0700

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -2,7 +2,7 @@
 
 username="$1"
 
-pkgs=('git' 'neofetch' 'virglrenderer-android' 'papirus-icon-theme' 'eza' 'pavucontrol-qt' 'bat' 'jq' 'nala' 'wmctrl' 'netcat-openbsd' 'termux-x11-nightly' 'eza')
+pkgs=('git' 'neofetch' 'termux-x11-nightly' 'papirus-icon-theme' 'eza' 'pavucontrol-qt' 'bat' 'jq' 'nala' 'wmctrl' 'netcat-openbsd' 'termux-x11-nightly' 'eza')
 
 #Install xfce4 desktop and additional packages
 pkg install "${pkgs[@]}" -y -o Dpkg::Options::="--force-confold"
